936bc51f03891b7d2dedc9bbfd2bd5cc6712b6d8
MarioDias470 <mario.sousa.dias@formandos.cinel.pt>
2026-07-28 00:20:20 +0100

    Init Web Project

---
+++ SalesCet1052.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesCet1052.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
